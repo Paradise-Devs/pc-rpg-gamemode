22ab28f3f9e315fb49936413caa46d5a7cee8de9
Larceny- <lcsdegodoy@outlook.com>
2016-01-11 17:01:49 -0200

    Adicionado sa-mp download ao launcher.

--- database/launcher.sql
+++ database/launcher.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 11, 2016 at 01:38 AM
+-- Generation Time: Jan 11, 2016 at 08:00 PM
 -- Server version: 10.1.8-MariaDB
 -- PHP Version: 5.6.14
 
@@ -28,21 +28,22 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `launcher` (
   `id` int(11) NOT NULL,
-  `version_major` int(11) NOT NULL,
-  `version_minor` int(11) NOT NULL,
-  `version_patch` int(11) NOT NULL,
+  `version` varchar(16) NOT NULL,
+  `download` varchar(256) NOT NULL,
   `ip` varchar(16) NOT NULL,
   `port` varchar(4) NOT NULL,
   `website` varchar(32) NOT NULL,
-  `frontpage` varchar(32) NOT NULL
+  `frontpage` varchar(32) NOT NULL,
+  `samp_version` varchar(16) NOT NULL,
+  `samp_download` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `launcher`
 --
 
-INSERT INTO `launcher` (`id`, `version_major`, `version_minor`, `version_patch`, `ip`, `port`, `website`, `frontpage`) VALUES
-(1, 0, 1, 0, '127.0.0.1', '7777', 'www.pc-rpg.com', 'www.pc-rpg.com/launcher');
+INSERT INTO `launcher` (`id`, `version`, `download`, `ip`, `port`, `website`, `frontpage`, `samp_version`, `samp_download`) VALUES
+(1, '0.1.0.0', 'https://s04.solidfilesusercontent.com/NzU0YmUwZDc0YjE4YjRjNzIwMjUxOTk4ZGM1ZDc2MzA2YzRmNzk1YzoxYUlnd2Y6RVF4M2dYOXBCNF9CNXRWZHlFZVNON0Rub3c4/bcdb77f9ef/ParadiseCityLauncher.exe', '127.0.0.1', '7777', 'www.pc-rpg.com', 'www.pc-rpg.com/launcher', '0, 3, 7, 0', 'http://files.sa-mp.com/sa-mp-0.3.7-install.exe');
 
 --
 -- Indexes for dumped tables
--- database/pc-rpg.sql
+++ database/pc-rpg.sql
@@ -132,21 +132,22 @@ CREATE TABLE `attachments` (
 
 CREATE TABLE `launcher` (
   `id` int(11) NOT NULL,
-  `version_major` int(11) NOT NULL,
-  `version_minor` int(11) NOT NULL,
-  `version_patch` int(11) NOT NULL,
+  `version` varchar(16) NOT NULL,
+  `download` varchar(256) NOT NULL,
   `ip` varchar(16) NOT NULL,
   `port` varchar(4) NOT NULL,
   `website` varchar(32) NOT NULL,
-  `frontpage` varchar(32) NOT NULL
+  `frontpage` varchar(32) NOT NULL,
+  `samp_version` varchar(16) NOT NULL,
+  `samp_download` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `launcher`
 --
 
-INSERT INTO `launcher` (`id`, `version_major`, `version_minor`, `version_patch`, `ip`, `port`, `website`, `frontpage`) VALUES
-(1, 0, 1, 0, '127.0.0.1', '7777', 'www.pc-rpg.com', 'www.pc-rpg.com/launcher');
+INSERT INTO `launcher` (`id`, `version`, `download`, `ip`, `port`, `website`, `frontpage`, `samp_version`, `samp_download`) VALUES
+(1, '0.1.0.0', 'https://s04.solidfilesusercontent.com/NzU0YmUwZDc0YjE4YjRjNzIwMjUxOTk4ZGM1ZDc2MzA2YzRmNzk1YzoxYUlnd2Y6RVF4M2dYOXBCNF9CNXRWZHlFZVNON0Rub3c4/bcdb77f9ef/ParadiseCityLauncher.exe', '127.0.0.1', '7777', 'www.pc-rpg.com', 'www.pc-rpg.com/launcher', '0, 3, 7, 0', 'http://files.sa-mp.com/sa-mp-0.3.7-install.exe');
 
 --
 -- Indexes for dumped tables
